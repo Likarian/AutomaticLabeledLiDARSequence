eff9864cbcbb7507fac8d44a625ce62fc8ecb33b
Wonjik Kim <44762643+Likarian@users.noreply.github.com>
2020-03-10 17:24:39 +0900

    Update DownloadAndUnzip.sh

--- DownloadAndUnzip.sh
+++ DownloadAndUnzip.sh
@@ -2,6 +2,7 @@ wget http://www.ok.sc.e.titech.ac.jp/res/LHD/data/TrainData.tar.gz
 wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/TrainDataAddit.tar.gz
 wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/TestSet.tar.gz
 
-tar -zxvf ./HumanDetectionVer02.tar.gz
-tar -zxvf ./TestsetForVer02.tar.gz
+tar -zxvf ./TrainData.tar.gz
+tar -zxvf ./TrainDataAddit.tar.gz
+tar -zxvf ./TestSet.tar.gz
 
