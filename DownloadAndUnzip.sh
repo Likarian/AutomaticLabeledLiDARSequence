a69267df55986c4232b0e01a7934d106d35bc005
Wonjik Kim <44762643+Likarian@users.noreply.github.com>
2020-03-10 17:24:16 +0900

    Update DownloadAndUnzip.sh

--- DownloadAndUnzip.sh
+++ DownloadAndUnzip.sh
@@ -1,4 +1,4 @@
-wget 
+wget http://www.ok.sc.e.titech.ac.jp/res/LHD/data/TrainData.tar.gz
 wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/TrainDataAddit.tar.gz
 wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/TestSet.tar.gz
 
